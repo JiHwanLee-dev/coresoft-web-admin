b572c6504b71a33cab0ac2c1efaa3682900f1563
JihwanLee <kdgosu53@gmail.com>
2020-09-25 09:12:51 +0900

    test_commit

--- src/components/header.vue
+++ src/components/header.vue
@@ -29,7 +29,7 @@
             <v-icon>mdi-note</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>회사 연혁</v-list-item-title>
+            <v-list-item-title>회사 연혁  </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
          <v-list-item link
